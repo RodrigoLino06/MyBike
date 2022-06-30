88dcf4ec473ee4e72fe48acb9920ecb1ddf2d8d0
RodrigoLino06 <digo-lino@live.com>
2022-06-30 17:17:23 -0300

    commit aí

--- app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
@@ -1,14 +1,38 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView cardClient, cardReservation, cardMaintenance, cardSales, cardAccessories, cardCatalog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+
+        cardClient = findViewById(R.id.cardClient);
+        cardReservation = findViewById(R.id.cardReservation);
+        cardMaintenance = findViewById(R.id.cardMaintenance);
+        cardSales = findViewById(R.id.cardSales);
+        cardAccessories = findViewById(R.id.cardAccessories);
+        cardCatalog = findViewById(R.id.cardCatalog);
+
+        cardClient.setOnClickListener(this);
+        cardReservation.setOnClickListener(this);
+        cardMaintenance.setOnClickListener(this);
+        cardSales.setOnClickListener(this);
+        cardAccessories.setOnClickListener(this);
+        cardCatalog.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/br/com/senac/mybike/ReservaActivity.java
@@ -0,0 +1,14 @@
+package br.com.senac.mybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reserva_layout);
+    }
+}
