36c9f540f9c0c9a53336afb84bb964d700244117
RodrigoLino06 <digo-lino@live.com>
2022-07-01 14:30:23 -0300

    outro commit aí

--- app/src/main/java/br/com/senac/mybike/ReservaActivity.java
+++ app/src/main/java/br/com/senac/mybike/ReservaActivity.java
@@ -3,12 +3,29 @@ package br.com.senac.mybike;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ReservaActivity extends AppCompatActivity {
+
+    FloatingActionButton fabReservar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reserva_layout);
+
+        fabReservar = findViewById(R.id.fabReservar);
+
+        fabReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Reserva realizada!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
